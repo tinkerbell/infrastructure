f8c5e69521a4f1af725f7288d2c893f9d450e9bf
David McKay <david@rawkode.com>
2021-03-22 15:08:42 +0000

    feat: configure domain for asset delivery

--- pulumi/src/assets/main.go
+++ pulumi/src/assets/main.go
@@ -7,15 +7,14 @@ import (
 )
 
 type AssetsDns struct {
-	Cname string
+	Cname *ns1.Record
 }
 
 func CreateAssetsDns(ctx *pulumi.Context, infrastructure internal.Infrastructure) (AssetsDns, error) {
-	// Create DNS record for Teleport
 	// This verification record is required for Cloudflare to issue a certificate for this domain
 	_, err := ns1.NewRecord(ctx, "assets-cname-verification", &ns1.RecordArgs{
 		Zone:   pulumi.String(infrastructure.Zone.Zone),
-		Domain: pulumi.String("_f213f2a073773bab6da1e978b94e1a92.assets"),
+		Domain: pulumi.String("_f213f2a073773bab6da1e978b94e1a92.assets.tinkerbell.org"),
 		Type:   pulumi.String("CNAME"),
 		Answers: ns1.RecordAnswerArray{
 			ns1.RecordAnswerArgs{
@@ -28,7 +27,26 @@ func CreateAssetsDns(ctx *pulumi.Context, infrastructure internal.Infrastructure
 		return AssetsDns{}, err
 	}
 
+	// assets.tinkerbell.org
+	// Because this Cloudfront/S3 distribution isn't configured on a CNCF
+	// account, it's been created outwidth this automation.
+	// So we're just linking them together. It's not ideal.
+	cname, err := ns1.NewRecord(ctx, "assets-cname", &ns1.RecordArgs{
+		Zone:   pulumi.String(infrastructure.Zone.Zone),
+		Domain: pulumi.String("assets.tinkerbell.org"),
+		Type:   pulumi.String("CNAME"),
+		Answers: ns1.RecordAnswerArray{
+			ns1.RecordAnswerArgs{
+				Answer: pulumi.String("d3b2o8hr2qjhyx.cloudfront.net"),
+			},
+		},
+	})
+
+	if err != nil {
+		return AssetsDns{}, err
+	}
+
 	return AssetsDns{
-		Cname: "test",
+		Cname: cname,
 	}, nil
 }
